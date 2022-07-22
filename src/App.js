47d60a01d167383b5af33fbc1b71bc9cb1e067c3
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-07-22 10:52:43 +0300

    prog

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import ToDoList from "./ToDoList";
-import React, { useState} from "react";   //{useState} <- use state hook, used in rerendering
+import React, { useState, useRef} from "react";   //{useState} <- use state hook, used in rerendering
+// {useRef} <- use references to HTML elements
 
 function App() {
-  const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false}])  // object destructuring; todos are the objects, setTodos is the function
+  const [todos, setTodos] = useState([])  // object destructuring; todos are the objects, setTodos is the function
+  const todoNameRef = useRef()
+  
+  function handleAddTodo(evt){
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+  }
+
   return (
     <>
     <ToDoList todos ={todos} />
-    <input type="text" />
-    <button>Add Todo</button>
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add Todo</button>
     <button>Clear Completed Todos</button>
     <div>0 left to do</div>
     </>
