8d50751c10b8fc64f97dadf85cdbbd01e5ca7d15
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2022-07-22 10:20:10 +0300

    object destructuring

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ToDoList from "./ToDoList";
 import React, { useState} from "react";   //{useState} <- use state hook, used in rerendering
 
 function App() {
+  const [todos, setTodos] = useState([])  // object destructuring; todos are the objects, setTodos is the function
   return (
     <>
     <ToDoList />
